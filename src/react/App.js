f02a7f9687dfed3fbd62f66df76b2906fa35bcb3
Gaurav Raj Sinha <gauravraj.sinha@singleinterface.com>
2020-09-12 10:39:23 +0530

    added header

--- src/react/App.js
+++ src/react/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import Header from './components/common/header/Header'
 
 export default function App() {
   return (
     <div className="App">
-      Hello
+        <Header />
     </div>
   )
 }
---
+++ src/react/components/common/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+import TimerIcon from '@material-ui/icons/Timer';
+
+function Header() {
+    return (
+        <div className="Header__Schedule">
+            <h1>Schedule <TimerIcon /></h1>
+        </div>
+    )
+}
+
+export default Header
